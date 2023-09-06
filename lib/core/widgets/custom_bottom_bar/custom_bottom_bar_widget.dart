38c17259cb207272b72ff99849c87192617792a2
lknagato <lukas_augusto@live.com>
2023-09-05 23:30:58 -0300

    modificaçoes no bottom bar

--- lib/core/widgets/custom_bottom_bar/custom_bottom_bar_widget.dart
+++ lib/core/widgets/custom_bottom_bar/custom_bottom_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:asp/asp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'atom/indexNavegation_atom.dart';
 
 class CustomBottomBarWidget extends StatefulWidget {
@@ -17,20 +18,18 @@ class _CustomBottomBarWidgetState extends State<CustomBottomBarWidget> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          _buildNavItem(Icons.home, 'Cartão', 0),
-          _buildNavItem(
-            Icons.search,
-            ' Conta',
-            1,
-          ),
-          _buildNavItem(Icons.favorite, ' Atividade', 2),
-          _buildNavItem(Icons.settings, ' Perfil', 3),
+          _buildNavItem(Icons.home, 'Cartão', 0, () {}),
+          _buildNavItem(Icons.search, ' Conta', 1, () {}),
+          _buildNavItem(Icons.favorite, ' Atividade', 2, () {
+            Modular.to.navigate('/limite');
+          }),
+          _buildNavItem(Icons.settings, ' Perfil', 3, () {}),
         ],
       ),
     );
   }
 
-  Widget _buildNavItem(IconData icon, String label, int index) {
+  Widget _buildNavItem(IconData icon, String label, int index, Function onTap) {
     bool isSelected = index == indexNavegation.value;
 
     return RxBuilder(
